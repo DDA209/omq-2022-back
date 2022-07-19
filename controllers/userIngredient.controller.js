9087c8c008d9e24337b6ed290d370f95ca5dd0f3
DDA209 <didier.pascarel1@gmail.com>
2022-07-19 23:51:42 +0200

    Finish controller/middleware userIngredients
    Finishing getting datas from front-end and storage un database for userIngredients datas and adding

--- controllers/userIngredient.controller.js
+++ controllers/userIngredient.controller.js
@@ -14,10 +14,7 @@ require('../middlewares/core/defaultMiddlewares')(router, here, Model);
 const plurialize = require('pluralize');
 const User = require(`../models/user.model`);
 const Ingredient = require(`../models/ingredient.model`);
-const getAllDocuments = require('../middlewares/getAllDocuments');
 const checkDocumentsDatas = require('../utils/checkDocumentsDatas');
-const createPopulationTree = require('../utils/createPopulationTree');
-const util = require('util');
 
 router.get('/user', (req, res) => {
 	console.log(`Specific controllers - GET /${plurialize(here)}/user`);
@@ -29,35 +26,11 @@ router.get('/user', (req, res) => {
 		'controllers/userIndredient #router GEt /user - req.query >>>',
 		req.query
 	);
-	// const { user: email } = req.body;
 	const query = { ...req.query };
-	// User.findOne({ email })
-	// 	.select('_id')
-	// 	.exec((err, userId) => {
-	// 		/* errors check */
-	// 		let theError = checkDocumentsDatas(err, userId, here);
-	// 		if (theError) {
-	// 			const success = false;
-	// 			const data = theError.toString();
 
-	// 			const response = { success, data };
-
-	// 			if (!res) {
-	// 				return response;
-	// 			} else {
-	// 				console.info(response);
-	// 				return res.json(response);
-	// 			}
-	// 		}
-	// 		/* end errors check */
-	// 		const user = userId;
-
-	// 		/* getting user's igredients */
 	skip = 0;
 	limit = false;
 
-	// Model.find({ user }, '_id ingredient quantity', {
-	// Model.find({ query }, '_id ingredient quantity', {
 	const { user } = req.query;
 	Model.find(
 		{ user },
@@ -72,7 +45,6 @@ router.get('/user', (req, res) => {
 			console.log('!!!!!!!!! document', document);
 			theError = checkDocumentsDatas(err, document, here);
 			if (theError) {
-				// success = false;
 				const success = false;
 				const data = theError.toString();
 				const response = { success, data };
